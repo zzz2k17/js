087644fb79eac792107016fa1b4360c4e27c4b19
Андрей <moto2k5@yandex.ru>
2021-06-27 09:28:30 +0300

    add button clear

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let myButton = document.querySelector('.my-button')
 let oneInput = document.querySelector('.oneinput')
 let twoInput = document.querySelector('.twoinput')
-let myHeader = document.querySelector('.header')
+let myResult = document.querySelector('.result')
+let clearButton = document.querySelector('.clearbutton')
 
 myButton.addEventListener('click', function() {
     
     let oneinputvalue = parseInt(oneInput.value)
     let twoinputvalue = parseInt(twoInput.value)
     
-    myHeader.innerText = oneinputvalue + twoinputvalue
+    myResult.innerText = oneinputvalue + twoinputvalue
 
+})
 
-
+clearButton.addEventListener('click', function() {
+    
+    myresult.innerText = " "
 })
